ead3a1ea7672d6f0c0095574d00e8f5f380db741
orange <bitter.tea.sweet.orange@gmail.com>
2023-07-06 19:35:52 +0800

    refactor: EasyHeadCell

---
+++ src/EasyHeadCell/index.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta } from '@storybook/react'
+import { useState } from 'react'
+import { EasyHeadCell } from '.'
+
+const meta = {
+  title: 'component/EasyHeadCell',
+  component: EasyHeadCell,
+} satisfies Meta<typeof EasyHeadCell>
+
+export default meta
+
+export const Defalut = () => {
+  return (
+    <EasyHeadCell width={200}>
+      height is 56, includes bottom border
+    </EasyHeadCell>
+  )
+}
+
+export const AlignRight = () => {
+  return (
+    <EasyHeadCell width={200} align="right">
+      align right
+    </EasyHeadCell>
+  )
+}
+
+export const Setting = () => {
+  return (
+    <EasyHeadCell
+      width={200}
+      showSettingIcon
+      onHideColumn={() => {
+        alert('hide column')
+      }}
+    >
+      show setting icon on hover
+    </EasyHeadCell>
+  )
+}
+
+export const Sortable = () => {
+  const [sort, setSort] = useState<'asc' | 'desc' | 'none'>('asc')
+  return (
+    <EasyHeadCell
+      width={200}
+      sortIO={{
+        value: sort,
+        onChange: setSort,
+      }}
+    >
+      sort
+    </EasyHeadCell>
+  )
+}
+
+export const Resize = () => {
+  const [width, setWidth] = useState<number>(100)
+  return (
+    <EasyHeadCell width={width} onWidthChange={setWidth}>
+      resize me
+    </EasyHeadCell>
+  )
+}
---
+++ src/EasyHeadCell/index.tsx
@@ -0,0 +1,166 @@
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
+import { Box, BoxProps, IconButton, useTheme } from '@mui/material'
+import { ResizableBox } from 'react-resizable'
+import { UseIOReturn } from 'react-utils-ts'
+import { EasyCell } from '../EasyCell'
+import { EasyTableHeadItemSetting } from '../head/EasyTableHeadItemSetting'
+
+export interface EasyHeadCellProps extends Omit<BoxProps, 'width'> {
+  width: number
+  /**
+   * resize
+   * */
+  onWidthChange?: (width: number) => void
+  /**
+   * @default 'left'
+   * */
+  align?: 'left' | 'center' | 'right'
+  /**
+   * if passed, the column will be sortable,
+   * */
+  sortIO?: UseIOReturn<EasyHeadCellSort>
+  showSettingIcon?: boolean
+  onHideColumn?: () => void
+}
+
+export type EasyHeadCellSort = 'asc' | 'desc' | 'none'
+
+export const HEAD_HEIGHT = 56
+export function EasyHeadCell({
+  width,
+  onWidthChange,
+  sx,
+  ...props
+}: EasyHeadCellProps) {
+  const { palette } = useTheme()
+  return onWidthChange ? (
+    <ResizableBox
+      style={{ position: 'relative', ...(sx as React.CSSProperties) }}
+      width={width}
+      height={HEAD_HEIGHT}
+      minConstraints={[100, HEAD_HEIGHT]}
+      maxConstraints={[1000, HEAD_HEIGHT]}
+      onResize={(_, data) => {
+        onWidthChange?.(data.size.width)
+      }}
+      handle={
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 20,
+            right: 0,
+            width: 16,
+            height: 16,
+            cursor: 'col-resize',
+            display: 'flex',
+            justifyContent: 'center',
+            '&:hover': {
+              '& div': {
+                background: palette.primary.main,
+              },
+            },
+          }}
+        >
+          <Box
+            sx={{
+              cursor: 'col-resize',
+              width: 2,
+              height: 16,
+              background: palette.divider,
+            }}
+          ></Box>
+        </Box>
+      }
+    >
+      <Content {...props} width={width} />
+    </ResizableBox>
+  ) : (
+    <Content {...props} width={width} />
+  )
+}
+
+function Content(props: EasyHeadCellProps) {
+  const {
+    showSettingIcon,
+    sortIO,
+    children: headerName,
+    align = 'left',
+    onHideColumn,
+    ...restProps
+  } = props
+  return (
+    <EasyCell
+      {...restProps}
+      height={HEAD_HEIGHT}
+      sx={{
+        display: 'flex',
+        flexDirection: align === 'right' ? 'row-reverse' : 'row',
+        position: 'relative',
+        fontWeight: 'bold',
+        alignItems: 'center',
+        justifyContent:
+          align === 'left' ? 'start' : align === 'right' ? 'end' : 'center',
+        '&:hover': {
+          '& button': {
+            visibility: 'visible',
+          },
+          '& svg': {
+            visibility: 'visible',
+          },
+        },
+      }}
+    >
+      {headerName}
+      {sortIO && (
+        <IconButton
+          onClick={() => {
+            const { value, onChange } = sortIO
+            switch (value) {
+              case 'none':
+                onChange?.('asc')
+                return
+              case 'asc':
+                onChange?.('desc')
+                return
+              case 'desc':
+                onChange?.('none')
+                return
+              default:
+                onChange?.('none')
+            }
+          }}
+          size="small"
+        >
+          {sortIO.value === 'none' && (
+            <ArrowUpwardIcon
+              sx={{
+                visibility: 'hidden',
+              }}
+              fontSize="small"
+              color="disabled"
+            />
+          )}
+          {sortIO.value === 'asc' && (
+            <ArrowUpwardIcon fontSize="small" color="primary" />
+          )}
+          {sortIO.value === 'desc' && (
+            <ArrowDownwardIcon fontSize="small" color="primary" />
+          )}
+        </IconButton>
+      )}
+      {showSettingIcon && (
+        <EasyTableHeadItemSetting
+          onHideColumn={onHideColumn}
+          onManageColumns={() => {
+            // TODO:
+            console.log()
+          }}
+          sx={{
+            visibility: 'hidden',
+          }}
+        />
+      )}
+    </EasyCell>
+  )
+}
